1e7c4afecd4df1ae2016f8834db83b80da5f056f
Nifemi <oluwaladen@gmail.com>
2023-03-28 21:35:48 +0100

    Css

---
+++ script.js
@@ -0,0 +1,5 @@
+/* ========================================= * 
+		        BEST VIEWED FULLSCREEN
+   CONTACT FOR MORE DESGIN
+   +2349065431332, IG: https://www.instagram.com/_ohluwanife/
+ * ========================================= */
